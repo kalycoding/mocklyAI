327686f3d9266877a2751386709575be1470e29f
hassan <hassanjaeh@gmail.com>
2025-05-22 22:00:27 +0100

    made navbar responsive

--- src/screens/Web/sections/NavBar/NavBar.jsx
+++ src/screens/Web/sections/NavBar/NavBar.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavBarWrapper } from "../../../../components/NavBarWrapper";
 import "./style.css";
 
 export const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <NavBarWrapper
-      buttonClassName="design-component-instance-node"
-      className="nav-bar-instance"
-      logo="/img/logo-1.svg"
-    />
+    <div className="nav-bar-container">
+      <NavBarWrapper
+        buttonClassName="design-component-instance-node"
+        className="nav-bar-instance"
+        logo="/img/logo-1.svg"
+      />
+      <button className="hamburger-menu" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      {isMenuOpen && (
+        <div className="mobile-menu">
+          <a href="#how-it-works">How It Works</a>
+          <a href="#features">Features</a>
+          <a href="#who-its-for">Who It's For</a>
+          <a href="#about-us">About Us</a>
+          <a href="#contact-us">Contact Us</a>
+        </div>
+      )}
+    </div>
   );
 };
