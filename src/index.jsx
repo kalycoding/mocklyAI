a3688ffb11c4cb00f6d8eb4a92359d43c61de802
kalycoding <kalycodes@gmail.com>
2025-06-09 00:17:55 +0100

    minor update

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Web } from "./screens/Web";
-import { Analytics } from '@vercel/analytics/next';
 
 createRoot(document.getElementById("app")).render(
   <StrictMode>
-    <Analytics />
     <Web />
   </StrictMode>
 );
